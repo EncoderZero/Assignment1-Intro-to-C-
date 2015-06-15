a5be6b7903adb75186d0d1768a0cc3645fe0d408
Kevin <intrepidhonor@gmail.com>
2015-06-15 14:23:11 -0400

    part 1 finished

--- Assignment 1/Fraction Calc.cpp
+++ Assignment 1/Fraction Calc.cpp
@@ -5,57 +5,117 @@
  */
 #include <iomanip>
 #include <iostream>
+#include <cctype>
 #include <cmath>
+#include <cstring>
 #include <string>
-
 using namespace std;
+int const STR_SIZE = 1000;
+int const ADJ_DELIMETER = 1;
+char const SEARCH_DELIM = '/';
+int const TBL_WD = 10;
+/* greatesetCommonFactor gets the greatest common factor
+* Parameter: int num1, numerator of the fraction
+* Parameter: int den1, denominator of the fraction
+* Returns int comFac, the greatest common factor
+*/
+int greatesetCommonFactor(int fact1, int fact2)
+{
+	int comFac = 1;
+	for (int i = 1; i <= fact1&&i <= fact2; i++)
+	{
+		if (fact1%i == 0 & fact2%i == 0)
+		{
+			comFac = i;
+		}
+	}
+	return comFac;
+}
+
+/* reduceFrac gets the most reduce form of the fraction
+* Parameter: int num1, numerator of the fraction
+* Parameter: int den1, denominator of the fraction
+* Returns: string full, the fraction in lowest terms.
+*/
+string reduceFrac(int num1, int den1)
+{
+	string full;
+	if (num1%den1 == 0){
+		full = to_string((num1 / den1));
+	}
+	else{
+		int comDiv = greatesetCommonFactor(num1, den1);
+		full = to_string(num1 / comDiv) + "/" + to_string(den1 / comDiv);
+	}
+
+	return full;
+}
+
 /*addFrac adds two fractions together and returns the results
 *
 */
-char* addFrac(char *frac1, char *frac2)
+string addFrac(int num1,int den1,int num2, int den2)
 {
-	char *sum;
-	return sum;
+	int sumNum, sumDen;
+	sumNum = (num1*den2) + (num2*den1);
+	sumDen = den2*den1;
+
+	return reduceFrac(sumNum,sumDen);
 }
 
 /*subtractFrac subtracts two fractions
 *
 */
-char* subtractFrac(char *frac1, char *frac2)
+string subtractFrac(int num1, int den1, int num2, int den2)
 {
-	char *sum;
-	return sum;
+	int sumNum, sumDen;
+	sumNum = (num1*den2) - (num2*den1);
+	sumDen = den2*den1;
+
+	return reduceFrac(sumNum, sumDen);
 }
 
 /*multiplyFrac multiplies two fractions
 */
-char* multiplyFrac(char *frac1, char *frac2)
+string multiplyFrac(int num1, int den1, int num2, int den2)
 {
-	char *product;
-	return product;
+	int prodNum, prodDen;
+	prodNum = num1*num1;
+	prodDen = den1*den2;
+	return reduceFrac(prodNum,prodDen);
 }
 
 /* divFrac divides two fractions
 */
-char* divFrac(char *frac1, char *frac2)
+string divFrac(int num1, int den1, int num2, int den2)
 {
-	char *quotient;
-	return quotient;
-}
-
-/* reduceFrac reduces the fraction into lowest value
-*/
-string reduceFrac(char *frac1)
-{
-	atoi("66");
-	int numerator, denominator;
-	string full;
-
-	return full;
+	int quotientNumerator, quotientDenominator;
+	quotientNumerator = num1*den2;
+	quotientDenominator = den1*num2;
+	return reduceFrac(quotientNumerator,quotientDenominator);
 }
 
 //main funciton to run
 int main()
 {
-
+	char *save;
+	char frac1[STR_SIZE], frac2[STR_SIZE];
+	int num1, num2, den1, den2;
+	cout << "First fractions: \n";
+	cin.getline(frac1, STR_SIZE);
+	cout << "Second fractions: \n";
+	cin.getline(frac2, STR_SIZE);
+    string frac3 = frac1;//convert to string to delemite the numberator from denominator as strtok fails on VS 2013
+	string frac4 = frac2;
+	num1 = stoi(frac3.substr(0, frac3.find(SEARCH_DELIM)));
+	den1 = stoi(frac3.substr(frac3.find(SEARCH_DELIM) + 1));
+	num2 = stoi( frac4.substr(0, frac4.find(SEARCH_DELIM)));
+	den2 = stoi(frac4.substr(frac4.find(SEARCH_DELIM) + 1));
+	cout << setw(TBL_WD) << "Addition" << setw(TBL_WD) << "Subtract" << setw(TBL_WD) << "Multiply" << setw(TBL_WD) << "Divide" << endl;
+	cout << setw(TBL_WD) << addFrac(num1, den1, num2, den2);
+	cout << setw(TBL_WD) << subtractFrac(num1, den1, num2, den2);
+	cout << setw(TBL_WD) << multiplyFrac(num1, den1, num2, den2);
+	cout << setw(TBL_WD) << divFrac(num1, den1, num2, den2) << endl;
+	system("pause");
+	return 0;
 }
